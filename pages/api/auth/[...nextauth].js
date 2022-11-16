067548b9bb227d3a1a1280469d2d2900d5fb75d8
Georges Yared <georges_yared99@hotmail.com>
2022-11-16 16:43:02 +0200

    Implement the next-auth

---
+++ pages/api/auth/[...nextauth].js
@@ -0,0 +1,13 @@
+import NextAuth from "next-auth"
+import RedditProvider from "next-auth/providers/reddit"
+export const authOptions = {
+  // Configure one or more authentication providers
+  providers: [
+    RedditProvider({
+      clientId: process.env.REDDIT_ID,
+      clientSecret: process.env.REDDIT_SECRET,
+    }),
+    // ...add more providers here
+  ],
+}
+export default NextAuth(authOptions)
